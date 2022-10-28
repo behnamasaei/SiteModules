e2fd8766c4b14bf85bec04280264c7cafc3b12fa
behnamasaei <behnamasaei@gmail.com>
2022-10-28 17:38:13 +0330

    generic repo

---
+++ BlogApi/BlogApi.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using BlogApi.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetPosts();
+        Task<T> GetPostById(Guid id);
+        Task<T> GetPostByName(string name);
+        Task<HttpResponse> AddPost(T entity);  
+        Task<HttpResponse> DeletePost(T entity);
+        Task<HttpResponse> UpdatePost(T entity);
+        Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ BlogApi/BlogApi.Domain/Models/HttpResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Domain.Models
+{
+    public class HttpResponse
+    {
+        public string StatusCode { get; set; }
+        public string Content { get; set; }
+
+    }
+}
